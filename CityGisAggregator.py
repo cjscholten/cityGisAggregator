77f6fea62515b158dfc5245fd705e034f279af9c
Chiel-Jan Scholten <cjscholten@gmail.com>
2016-01-17 18:37:46 +0100

    json

--- CityGisAggregator.py
+++ CityGisAggregator.py
@@ -53,7 +53,7 @@ class CityGisAggregator:
 		#data = demjson.encode(['meting_type': 'CS','voertuig_id': null ,'meting_datum':date, 'waarde': success, 'unit_id': unit_id])
         
 		succesDate = date
-		failedDate = date
+		failedDate = succesDate
         
 		failedJson = [ { 'meting_type' : 'CF', 'voertuig_id' : 0, 'meting_datum' : successDate, 'waarde' : failed, 'unit_id' : unit_id } ]
         succesJson = [ { 'meting_type' : 'CS', 'voertuig_id' : 0, 'meting_datum' : failedDate, 'waarde' : success, 'unit_id' : unit_id } ]
