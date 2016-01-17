d24235ecbcb5c7bd2e73b5d0cec31bb8f5b50b3b
Chiel-Jan Scholten <cjscholten@gmail.com>
2016-01-17 18:47:12 +0100

    json

--- CityGisAggregator.py
+++ CityGisAggregator.py
@@ -52,12 +52,13 @@ class CityGisAggregator:
 		#succesvolle connections 
 		#data = demjson.encode(['meting_type': 'CS','voertuig_id': null ,'meting_datum':date, 'waarde': success, 'unit_id': unit_id])
         		        
-		#failedJson = [ { 'meting_type' : 'CF', 'voertuig_id' : 0, 'meting_datum' : date, 'waarde' : failed, 'unit_id' : unit_id } ]
-        succesJson = [ { 'meting_type' : 'CS', 'voertuig_id' : -1, 'meting_datum' : date, 'waarde' : success, 'unit_id' : unit_id } ]
+		failedJson = [ { 'meting_type' : 'CF', 'voertuig_id' : -1, 'meting_datum' : date, 'waarde' : failed, 'unit_id' : unit_id } ]
+                succesJson = [ { 'meting_type' : 'CS', 'voertuig_id' : -1, 'meting_datum' : date, 'waarde' : success, 'unit_id' : unit_id } ]
         
-        json1 = demjson.encode(succesJson)
-        json2 = demjson.encode(failedJson)
-        print json1, json2
+                json1 = demjson.encode(succesJson)
+                json2 = demjson.encode(failedJson)
+                print json1
+                print json2
 
 
 aggregate = CityGisAggregator()
