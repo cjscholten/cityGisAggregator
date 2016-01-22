5022284f2bbf7a442d85d1619eaaafb4bf9507f8
Chiel-Jan Scholten <cjscholten@gmail.com>
2016-01-22 21:29:18 +0100

    fixes structure

--- CityGisAggregator.py
+++ CityGisAggregator.py
@@ -13,30 +13,26 @@ class CityGisAggregator:
              passwd="Z32hKL",
              db="citygis")
 
-        self.db_aggregate = MySQLdb.connect(host="localhost",
-             user="root",
-             passwd="Z32hKL",
-             db="aggregate_results")
-
 
     def aggregateConnection(self):
-                
-        cur = self.db_citigis.cursor()   
-        #Haal alle waarden op voor foute connectie    
+
+        cur = self.db_citygis.cursor()
+
+        #Haal alle waarden op voor foute connectie
         cur.execute("select unit_id, DATE_FORMAT(`datetime`, '%Y-%m-%d'), count(*) from connection where `value` = 0 group by unit_id, DATE_FORMAT(`datetime`, '%Y-%m-%d')")
         failed = list(cur.fetchall())
         for row in failed:
             self.__insertMeting(row[0],row[1],row[2],'CF')
-            
-            
+
+
         #Haal alle waarden op voor goede connectie
         cur = self.db_citigis.cursor()
-          
+
         cur.execute("select unit_id, DATE_FORMAT(`datetime`, '%Y-%m-%d'), count(*) from connection where `value` = 1 group by unit_id, DATE_FORMAT(`datetime`, '%Y-%m-%d')")
         success = list(cur.fetchall())
         for row in success:
             self.__insertMeting(row[0],row[1],row[2],'CS')
-                
+
 
 
 
@@ -44,12 +40,12 @@ class CityGisAggregator:
     def __insertMeting(self, unit_id, date, waarde, metingType):
         url = "http://145.24.222.120/citygis/meting"
         insertJson =  { 'meting_type' : metingType, 'voertuig_id' : unit_id, 'meting_datum' : date, 'waarde' : waarde }
-        
+
         req = urllib2.Request(url, json.dumps(insertJson), headers={'Content-type': 'application/json', 'Accept': 'application/json'})
         response = urllib2.urlopen(req)
         print response.read()
 
-     
+
 
 
 aggregate = CityGisAggregator()
