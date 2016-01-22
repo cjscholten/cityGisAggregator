1aaa8987c3d83f9c87d1ab66da41dd5053897189
Chiel-Jan Scholten <cjscholten@gmail.com>
2016-01-22 22:35:57 +0100

    debug

--- CityGisAggregator.py
+++ CityGisAggregator.py
@@ -45,7 +45,9 @@ class CityGisAggregator:
         for day in days:
             totalDistance = 0
             cur = self.db_citygis.cursor()
-            cur.execute("select rdx, rdy from position where unit_id = " + str(day[0]) + " and DATE_FORMAT(`datetime`, '%Y-%m-%d') = " + day[1])
+            sql = "select rdx, rdy from position where unit_id = " + str(day[0]) + " and DATE_FORMAT(`datetime`, '%Y-%m-%d') = " + day[1]
+            print sql
+            cur.execute(sql)
             positions = list(cur.fetchall())
             posOud = [0,0]            
             posNew = [0,0]
